a886ec89731df20939c1b12bedc8ad1f1b00cea3
malik-13503 <95710499+malik-13503@users.noreply.github.com>
2024-11-03 02:08:01 +0500

    Add files via upload

---
+++ src/js/quizes.js
@@ -0,0 +1,173 @@
+questions = [
+  {
+    q: 'What is the emergency number to call if there is a fire?',
+    a: '990',
+    choices: ['811', '990', '911', '999']
+  },
+  {
+    q: 'What is the best way to escape a smoked-filled room?',
+    a: 'Stand up and run quickly',
+    choices: ['Stand up and run quickly', 'Crawl low on all' , 'Open windows to let the smoke out' , 'Cover your face and walk through the smoke']
+  },
+  {
+    q: 'What should you do if your clothes catch fire?',
+    a: 'Stop, Drop, and Roll',
+    choices: ['Run to a shower', 'Stop, Drop, and Roll', 'Call for help, run as fast as possible', 'Try to pull the clothes off']
+  },
+  {
+    q: 'What is the safest way to use a fire extinguisher?',
+    a: 'Aim at the base of the fire',
+    choices: [
+      'Aim at the base of the fire',
+      'Aim at the flames',
+      'Stand far away and spray',
+      'Swing the extinguisher in circles'
+    ]
+  },
+  // {
+  //   q: 'Which statement below is FALSE?',
+  //   a: 'Cylinders can be transported lying down in your vehicle',
+  //   choices: [
+  //     'Cylinders can be transported lying down in your vehicle',
+  //     'Cylinders must be stored in a well-ventilated location away from sources of ignition',
+  //     'If a leak is suspected, move the cylinder outdoors and cover it with a damp cloth',
+  //     'Cylinders should not be kept unused for more than 6 months'
+  //   ]
+  // },
+  {
+    q: 'What should you wear if you are evacuating during a bushfire?',
+    a: 'Long sleeves, pants, and sturdy shoes',
+    choices: [
+      'Loose, comfortable clothes',
+      'Light, breathable fabric',
+      'Long sleeves, pants, and sturdy shoes',
+      'Swimsuits and sandals'
+    ]
+  },
+
+  {
+    q: 'What percentage of your body is made up of water?',
+    a: '60%',
+    choices: [
+      '20%',
+      '90%',
+      '60%',
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'Which organ removes excess water and salt from the body?',
+    a: 'Kidneys',
+    choices: [
+      'Kidneys',
+      'Liver',
+      'Bladder',
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'How many chambers are there in the heart?',
+    a: '4',
+    choices: [
+      '6',
+      '4',
+      '2',
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'Which organ controls blood sugar levels?',
+    a: 'The pancreas',
+    choices: [
+      'The Kidneys',
+      'The pancreas',
+      'The liver',
+    
+    ]
+  }
+  ,
+
+  {
+    q: ' Which organ in the body produces cholesterol?',
+    a: 'Liver',
+    choices: [
+      'Pancreas',
+      'Thyroid',
+      'Liver',
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'What does ISO stand for?',
+    a: 'International Organization for Standardization',
+    choices: [
+      'Industrial Organization Standard',
+      ' Industry Original Standard',
+      'International Organization for Standardization',
+      'nternal Organization for Standardization'    
+    ]
+  }
+  ,
+
+  {
+    q: 'What does P.A.S.S stand for?',
+    a: 'Pull Aim Squeeze Sweep',
+    choices: [
+      'Push Aim Sweep Stand',
+      'Pull Assess Strain Ship',
+      'Pull Aim Squeeze Sweep',
+      'Press Aim Strip Stretch',    
+    ]
+  }
+  ,
+
+  {
+    q: 'What does ACE stand for?',
+    a: 'Alternate Corrective Entry',
+    choices: [
+      'Accredited Certification Exercise',
+      'Alarm Criteria Exception',
+      'Alert Company Emergency',
+      'Alternate Corrective Entry'
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'What are some factors that affect the lifespan of PPE?',
+    a: 'All of the above',
+    choices: [
+      'The quality of material used to manufacture them',
+      'Exposure to UV rays',
+      'Storage method',
+      'All of the above'
+    
+    ]
+  }
+  ,
+
+  {
+    q: 'When was the first HSE legislation developed?',
+    a: '1802',
+    choices: [
+      '1802',
+      '1722',
+      '1908',
+      '2010'
+    
+    ]
+  }
+
+  
+];
+
+
